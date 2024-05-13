e163ec73b59acf60541778f47e9593db6aca254f
alex2stf <alex2stf@yahoo.com>
2024-05-13 10:06:59 +0300

    music mode plugin

--- src/main/plugins/arise-chrome/main.js
+++ src/main/plugins/arise-chrome/main.js
@@ -7,6 +7,22 @@ var playerOutro = document.getElementById('player-container-outer');
 
 function continueScan(){
     clickAll('ytp-skip-ad-button');
+//    clickAll('yt-spec-touch-feedback-shape yt-spec-touch-feedback-shape--touch-response');//NU
+//yt-spec-touch-feedback-shape__fill
+//    clickAll('yt-spec-touch-feedback-shape__fill');
+
+//    var accepts = document.querySelectorAll('.yt-spec-touch-feedback-shape.yt-spec-touch-feedback-shape--touch-response-inverse .yt-spec-touch-feedback-shape__fill');
+//    for(var i = 0; i < accepts.length; i++) {
+//        var x = (accepts[i].innerHTML);
+//        if((x + "").toLowerCase().indexOf('accept all') > -1) {
+//            console.log(x)
+//        }
+//    }
+
+
+
+
+//    clickAll('ytp-large-play-button ytp-button');
     clickAll('ytp-ad-skip-button-container ytp-ad-skip-button-container-detached');
 }
 
---
+++ src/main/plugins/music-mode/abbpaepbpakcpipajigmlpnhlnbennna/6.4.2_0/config.js
@@ -0,0 +1,5 @@
+// Chrome configuration
+const support_url = "https://chromewebstore.google.com/detail/music-mode-for-youtube/abbpaepbpakcpipajigmlpnhlnbennna/support";
+const review_url = "https://chromewebstore.google.com/detail/music-mode-for-youtube/abbpaepbpakcpipajigmlpnhlnbennna/reviews";
+
+export {support_url, review_url};
