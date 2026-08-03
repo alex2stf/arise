81fbf96e0df603cb5e9df76100631fae2ea535f7
Alex Stefan <alex2stf@yahoo.com>
2026-08-03 18:46:28 +0300

    wip

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,6 +1,6 @@
 @echo off
 @rem Find java.exe
-@rem set JAVA_HOME=C:\Program/ Files\Java\jdk1.7.0_80\bin
+set JAVA_HOME=C:\Applications\jdk8-temurin\bin
 
 set JVEXE=%JAVA_HOME%\java.exe
 set JVC=%JAVA_HOME%\javac.exe
