07b903fb5ce4c9716e3764d983bc84ff00c5bba8
Alex Stefan <alex2stf@yahoo.com>
2026-08-28 17:57:37 +0300

    wip

--- src/main/java/com/arise/canter/CommandRegistry.java
+++ src/main/java/com/arise/canter/CommandRegistry.java
@@ -158,7 +158,7 @@ public enum CommandRegistry  {
         if (!isEmpty(args)){
             localArgs = new String[args.length];
             for (int i = 0; i < args.length; i++){
-                log.trace(i + " arg === " + args[i] + " at command " + commandId);
+                log.trace(i + " arg === " + args[i] + " at command " + commandId); //todo pune null check
                 String currentArg = args[i];
                 if (currentArg.startsWith("$")){
                     localArgs[i] = executeCommandLine(currentArg) + "";
