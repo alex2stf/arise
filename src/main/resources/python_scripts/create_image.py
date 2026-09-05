6b0346f38590ea9169eb828b86771cc97900f60b
Alex Stefan <alex2stf@yahoo.com>
2026-09-05 23:36:25 +0300

    wip

--- src/main/resources/python_scripts/create_image.py
+++ src/main/resources/python_scripts/create_image.py
@@ -6,16 +6,49 @@ HEIGHT = 1050
 #  = 1000
 # h = 1000
 
+def colors2():
+    return [
+        ['#2BF58B', '#F52B93']
+        ,['black', 'white']
+        ,['white', 'black']
+    ]
+
+#src at https://htmlcolorcodes.com/color-picker/
+def color_arrays():
+    return [
+        ['#44EB11', '#11EB4C', '#11EBB8']
+        ,['#9BEB11', '#2EEB11', '#11EB61']
+        ,['#E4EB11', '#77EB11', '#11EB18']
+        ,['#EBAA11', '#BFEB11', '#52EB11']
+        ,['#EB6F11', '#EBDC11', '#8DEB11']
+        ,['#EB6F11', '#EBDC11', '#8DEB11']
+        ,['#EB1C11', '#EB8911', '#E0EB11']
+        ,['#EB1140', '#EB4F11', '#EBBC11']
+        ,['#EB1177', '#EB1811', '#EB8511']
+        ,['#11CAEB', '#115DEB', '#3211EB']
+        ,['#DCB352', '#C0DC52', '#7BDC52', '#527BDC', '#B352DC']
+        ,['#B052DC', '#DC52C3', '#DC527E', '#7EDC52', '#52DCB0']
+        ,['#526BDC', '#7E52DC', '#C352DC', '#DCC352', '#6BDC52']
+        ,['#89DC52', '#52DC60', '#52DCA5', '#A552DC', '#DC5289']
+        ,['#DCBA52', '#BADC52', '#74DC52', '#5274DC', '#BA52DC']
+        ,['#41BA77', '#41BAB4', '#4184BA', '#BA4184', '#BA7741']
+    ]
+
+def colors4():
+    return [
+        '#8AD2F6', '#E48AF6', '#F6AE8A', '#9CF68A'
+    ]
+
+# out = Image.new("RGB", (WIDTH, HEIGHT), (255, 255, 255))
 
 
-out = Image.new("RGB", (WIDTH, HEIGHT), (255, 255, 255))
-draw = ImageDraw.Draw(out)
 
 
-
-def draw_sinusoid(d, color, w, h):
-    lw = 15
+def img_sinusoid(color, background, w, h, lw):
+    out = Image.new("RGB", (w, h), background)
+    d = ImageDraw.Draw(out)
     rmax = random.randint(10, 18)
+    print(rmax, ' sinuses ', lw, ' width')
     rmaxval = rmax - 1
     bw = w + lw
     wbase = bw / rmax
@@ -37,39 +70,106 @@ def draw_sinusoid(d, color, w, h):
             e = 180
         d.arc([( x0, y0), (x1, y1)], start = s, end = e, fill=color, width=lw)
         rmaxval = rmaxval - 1
+    return out
 
 
-draw_sinusoid(draw, 'blue', WIDTH, HEIGHT)
+def draw_lines(d, w, h, lw, colors):
+    clen = len(colors)
+    if clen % 2 == 0:
+        mid = random.choice(colors)
+    else:
+        mid = colors[clen // 2]
+    half = h / 2
+    d.line([-lw, half, w, half], fill=mid, width=lw)  #o diagonala
+    max = clen
+    print('max ', max)
 
-# d.arc([(-lw, h / 4), (w16, h - (h / 4) )], start = 0, end = 180, fill=color, width=lw)
-# d.arc([(w16 - lw, 0), ( w - (w16 * 14) , h)], start = 180, end = 0, fill=color, width=lw)
-# d.arc([( (w16 * 2) - lw, 0), ( w - (w16 * 13), h)], start = 0, end = 180, fill=color, width=lw)
-# d.arc([( (w16 * 3) - lw, h / 4), ( w - (w16 * 12), h)], start = 180, end = 0, fill=color, width=lw)
-# d.arc([( (w16 * 4) - lw, h / 4), ( w - (w16 * 11), h - (h / 6))], start = 0, end = 180, fill=color, width=lw)
-# d.arc([( (w16 * 5) - lw, h / 5), ( w - (w16 * 10) , h)], start = 180, end = 0, fill=color, width=lw)
-# d.arc([( (w16 * 6) - lw, h / 6), ( w - (w16 * 9) , h)], start = 0, end = 180, fill=color, width=lw)
-# d.arc([( (w16 * 7) - lw, h - (h / 2) ), ( w - (w16 * 8), h - (h / 4))], start = 0, end = 360, fill='cyan', width=lw)
-# d.arc([( (w16 * 8) - lw, (h / 2) - (h / 4) ), ( w - (w16 * 7), h - (h / 6) )], start = 0, end = 360, fill='green', width=lw)
+    step = lw * 2
+    for i in range(0, max):
+        d.line([-lw, half - step, w + lw, half - step], fill=colors[i], width=lw) # lines top
+        step = step + lw * 2
 
-# d.arc([( (w16 * 9) - lw, (h / 2) - (h / 4) ), ( w - (w16 * 6), h - (h / 8) )], start = 180, end = 0, fill='green', width=lw)
-# d.arc([( (w16 * 10) - lw, 0 ), ( w - (w16 * 5), h )], start = 0, end = 180, fill='blue', width=lw)
-# d.arc([( (w16 * 11) - lw, 0 ), ( w - (w16 * 4), h )], start = 180, end = 0, fill='green', width=lw)
-# d.arc([( (w16 * 12) - lw, (h / 2) - (h / 4) ), ( w - (w16 * 3), h - (h / 4))], start = 0, end = 180, fill=color, width=lw)
+    step = lw * 2
+    for i in range(0, max):
+        d.line([-lw, half + step, w + lw, half + step], fill=colors[i], width=lw)  #lines bottom
+        step = step + lw * 2
 
-# d.arc([(0, 0), (1680, 1050)], start = 0, end = 90, fill ="red") #la fel ca pieslice
+# draw_lines(draw, WIDTH, HEIGHT, 10)
 
-# d.line([0, 0, w, h], fill='green', width=40)  #o diagonala
+def draw_z(d, w, h, lw, fill, outline):
+    w6 = w / 6
+    w2 = w / 2
+    h2 = h / 2
+    triangle_vertices = [
+        (w2 + w6, 0),
+        (w2 - w6, h2),
+        (w2 + w6, h2),
+        (w2 - w6, h),
+        (w2, h2)
+    ]
+    d.polygon(
+        triangle_vertices,
+        fill=fill,     # Color inside the triangle
+        outline=outline,       # Color of the border
+        width=lw               # Border thickness in pixels
+    )
 
-#smiley
-# d.ellipse((0,0,90,90),'yellow','blue') #capul
-# d.ellipse((25,20,35,30),'yellow','blue') #ochi dreapta
-# d.ellipse((50,20,60,30),'yellow','blue') #ochi stanga
-# d.arc((20,40,70,70), 0, 180, 0) #gura
+def draw_z2(d, w, h, lw, fill, outline):
+    w6 = w / 6
+    w2 = w / 2
+    h2 = h / 2
+    h6 = h / 6
+    wf = 2
+    hf = 2
+    triangle_vertices = [
+        (w2 + w6, 0),
+        (w2 - w6 * wf, h2 + h6),
+        (w2 , h2 + h6 / hf),
+        (w2 - w6, h),
+        (w2 + w6 * wf, h2 - h6),
+        (w2, h2 - h6 / hf),
+        (w2 + w6, 0)
+    ]
+    d.polygon(
+        triangle_vertices,
+        fill=fill,     # Color inside the triangle
+        outline=outline,       # Color of the border
+        width=lw               # Border thickness in pixels
+    )
 
-# draw.line([10, 10, 120, 120], fill='red')
-# draw.rectangle((0, 0, 1, 1), (0, 255, 0, 127))
-# draw.chord([0, 0, 10, 10], fill='blue')
 
-out.save('generated.png')
+def img_with_lines(color, background, w, h, lw, line_colors):
+    print('lw=', lw)
+    out = Image.new("RGB", (w, h), background)
+    d = ImageDraw.Draw(out)
+    draw_lines(d, w, h, lw, line_colors) #ok
+    # draw_z2(d, w, h, 0, color, 'red') #ok
+    # d.ellipse([(20, 20), (w / 6, w / 6)], color) #ok, arata ca o luna
 
-out.show()
+    cw = lw * len(color) * random.choice([1, 1.5, 2])
+    w2 = random.choice(
+        [
+            w - cw / 2, w / 2, w / 1.5, w, w / random.randint(3, 9)
+        ]
+    )
+    h2 = h / 2
+    d.ellipse([( w2 - cw, h2 - cw ), (w2 + cw, h2 + cw)], color) #ok
+    return out
+
+# color = random.choice(colors3())
+# draw_z2(draw, WIDTH, HEIGHT, 20, color[0], color[1])
+
+# draw_lines(draw, WIDTH, HEIGHT, 20)
+
+# img = img_sinusoid('black', 'white', WIDTH, HEIGHT, 10)
+# img = img_with_lines('red', 'black', WIDTH, HEIGHT, random.randint(8, 15), ['magenta', 'blue', 'red'])
+img = img_with_lines(
+    random.choice(['#2E90FF', '#D40000', '#F5FF2E', '#2EFF49'
+                   '#2EFFEA', '#F12EFF', '#FFAB2E', '#FF662E', '#FF2E2E', '#4A2EFF', '#972EFF',
+                   '#D52EFF', '#FF2EBD', '#FF2E6D', '#00D40E', '#0019D4'
+                   ])
+, 'black', WIDTH, HEIGHT, random.randint(8, 15), ['white'] * random.randint(1, 5))
+
+img.save('generated.png')
+
+img.show()
